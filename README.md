f71142344b91925de57abf0c1e24a8b38086f82c
Key Compton <integra378@aol.com>
2021-07-11 18:51:58 -0400

    JS and readme mods

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Portfolio-3
+Created arrays for password character options
+---Created window prompts for each character option
+---Created password length parameters
+---
+To Do: Figure out how to make windows actually appear, determine where console.log is necessary, 
+implement a random character generator for each type of character
--- script.js
+++ script.js
@@ -5,6 +5,36 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+  var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  var passwordLength= "";
+
+  var passwordLength= window.prompt("How many characters would you like your password to consist of? It must be between 8 and 128 characters.");
+
+    console.log(passwordLength);
+
+    if(passwordLength <= 8 || passwordLength >= 128);
+
+    console.log("Please choose a character number between 8 and 128");
+
+
+    var addSpecial= window.prompt("Would you like to include special characters in your password?");
+
+    
+
+
+    var addNumber= window.prompt("Would you like to include numbers in your password?");
+
+
+    var addUppercase= window.prompt("Would you like to include uppercase letters in your password?");
+
+   
+    var addLowercase= window.prompt("Would you like to include lowercase letters in your password?");
+
+
 
   passwordText.value = password;
 
